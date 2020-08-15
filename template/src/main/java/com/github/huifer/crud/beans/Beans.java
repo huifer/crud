b7915f6801e589d23be3b0e42494fbd6b8011c20
huifer <huifer97@163.com>
2020-08-15 17:45:01 +0800

    fix: 修正组件扫描位置

--- template/src/main/java/com/github/huifer/crud/beans/Beans.java
+++ template/src/main/java/com/github/huifer/crud/beans/Beans.java
@@ -2,7 +2,7 @@ package com.github.huifer.crud.beans;
 
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"org.huifer.crud.*"})
+@ComponentScan(basePackages = {"com.github.huifer.crud.*"})
 public class Beans {
 
 }
